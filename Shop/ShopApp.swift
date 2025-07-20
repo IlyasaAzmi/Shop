2977289a35d19433a9bc10a2cf9ec6abdf031eaf
Ilyasa' Azmi <theazmi99ilyasa@gmail.com>
2025-07-20 10:40:49 +0700

    Initial Commit

---
+++ Shop/ShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShopApp.swift
+//  Shop
+//
+//  Created by Ilyasa' Azmi on 20/07/25.
+//
+
+import SwiftUI
+
+@main
+struct ShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ShopTests/ShopTests.swift
@@ -0,0 +1,17 @@
+//
+//  ShopTests.swift
+//  ShopTests
+//
+//  Created by Ilyasa' Azmi on 20/07/25.
+//
+
+import Testing
+@testable import Shop
+
+struct ShopTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
